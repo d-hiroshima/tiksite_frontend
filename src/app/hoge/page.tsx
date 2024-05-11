2b0cdff097aa37418e90a7975b981a6fac9fed06
Hiroshima Daisuke <hiroshima@tetemarche.co.jp>
2024-05-12 01:58:08 +0900

    feat: ちょっと整えた

--- src/app/hoge/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hoge() {
-  return (
-    <main>
-      <div>test</div>
-    </main>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import {
-  Button,
+  Card,
+  CardContent,
   Container,
-  Paper,
+  Grid,
+  Typography
 } from '@mui/material';
 // import styles from './page.module.css';
 
 export default function Home() {
   return (
     <Container>
-      <Paper elevation={3} sx={{ p: 5 }}>
-        <Button variant='contained' color='primary'>
-          ボタン
-        </Button>
-      </Paper>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                投稿数
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                20
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                フォロワー数
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                1,000
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                インプレッション数
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                1,000,000
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
